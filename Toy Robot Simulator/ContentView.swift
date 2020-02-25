7ea987d3e4719b929a7ca3d8eb462b8f9acc69b1
Tomas Friml <tomasfriml@Tomass-MBP-1825.fritz.box>
2020-02-25 21:41:33 +1300

    Initial project setup

---
+++ Toy Robot Simulator/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Toy Robot Simulator
+//
+//  Created by Tomas Friml on 25/02/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
